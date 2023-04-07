bcaef5f90e5e249360c3b8024e1913b5ebbe7d0f
genebit <genebitara@gmail.com>
2023-04-07 14:09:52 +0800

    Added breadcrumbs and moved to respective folders

---
+++ resources/views/auth/login/loginAsWorker.blade.php
@@ -0,0 +1,82 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+	<nav style="--bs-breadcrumb-divider: url('{{ asset('svg/breadcrumb-divider.svg') }}');" aria-label="breadcrumb">
+		<ol class="breadcrumb">
+			<li class="breadcrumb-item">
+				<a href="/login" class="fw-bolder">Login</a>
+			</li>
+			<li class="breadcrumb-item active" aria-current="page">As Worker</li>
+		</ol>
+	</nav>
+</div>
+<div class="container fade-in">
+    <div class="card shadow border border-1 rounded-5 p-5">
+        <div class="card-body px-5 py-4">
+            <div class="row px-5 py-4 gx-5">
+                <div class="col-md-6">
+                    <h1 class="text-uppercase fw-bolder">Login as worker</h1>
+                    <p>Don't have an account yet? 
+                        <a href="/register/worker" class="text-decoration-underline fw-bold">Sign Up</a>
+                    </p>
+                    <form method="POST" action="{{ route('login') }}">
+                        @csrf
+                        <div class="row mb-3">
+                            <label for="email" class="small fw-bold">Email Address</label>
+                            <div class="col-md-12">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong class="small text-danger">{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+                        <div class="row mb-3">
+                            <label for="password" class="small fw-bold">Password</label>
+
+                            <div class="col-md-12">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong class="small text-danger>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+                        <div class="d-flex mb-3 justify-content-between">
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+                                <label class="small" for="remember">
+                                    {{ __('Remember Me') }}
+                                </label>
+                            </div>
+                            <div>
+                                @if (Route::has('password.request'))
+                                <a class="small fw-bold text-decoration-underline" href="{{ route('password.request') }}">
+                                    {{ __('Forgot Your Password?') }}
+                                </a>
+                                @endif
+                            </div>
+                        </div>
+                        <div class="d-flex justify-content-center">
+                            <div></div>
+                            <button type="submit" class="btn btn-primary ms-auto text-uppercase fw-bold">Login <i class="fa-solid fa-arrow-right-long text-white"></i></button>
+                        </div>
+                    </form>
+                </div>
+                <div class="col-md-6 d-flex justify-content-center flex-column gap-3">
+                    <div class="text-center">
+                        <img src="{{ asset('svg/freepik-login.svg') }}" alt="" width="500">
+                    </div>
+                    <h5 class="text-uppercase fw-bolder">LINIG-ON, YOUR TRUSTED HOME SERVICE APPLICATION</h5>
+                    <p>Our team provides janitorial services that is easily available within your fingertips, forget the hassle of doing everything by yourself. Sign up now! </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/auth/register/registerAsWorkerInd.blade.php
@@ -0,0 +1,97 @@
+@extends('layouts.app') @section('content')
+<div class="container">
+	<nav style="--bs-breadcrumb-divider: url('{{ asset('svg/breadcrumb-divider.svg') }}');" aria-label="breadcrumb">
+		<ol class="breadcrumb">
+			<li class="breadcrumb-item">
+				<a href="/login" class="fw-bolder">Login</a>
+			</li>
+			<li class="breadcrumb-item">
+				<a href="/register/worker" class="fw-bolder">Register Worker</a>
+			</li>
+			<li class="breadcrumb-item active" aria-current="page">Solo</li>
+		</ol>
+	</nav>
+</div>
+<div class="container fade-in">
+	<div class="card shadow border border-1 rounded-5 p-0">
+		<div class="card-body p-0">
+			<div class="row">
+				<div class="col-md-2 ps-4 py-4">
+					<div class="ps-4 py-4">
+						<div class="row gap-3">
+							<div class="col-12">
+								<div class="d-flex flex-column gap-3">
+									<img class="img-fluid" src="{{ asset('svg/upload-photo.svg') }}" alt="" />
+									<button class="btn btn-primary w-100 fw-bold text-uppercase">Upload photo</button>
+									<button class="btn btn-secondary w-100 fw-bold text-uppercase">Upload resume</button>
+								</div>
+							</div>
+							<div class="col-12">
+								<label for="" class="small text-muted fw-bold">Gender</label>
+								<div class="row">
+									<div class="col-lg-6">
+										<div class="form-check">
+											<input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" />
+											<label class="form-check-label" for="flexRadioDefault1"> Male </label>
+										</div>
+									</div>
+									<div class="col-lg-6">
+										<div class="form-check">
+											<input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" />
+											<label class="form-check-label" for="flexRadioDefault2"> Female </label>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-5 px-3 py-4">
+					<div class="px-3 py-4">
+						<h1 class="text-uppercase fw-bolder">Register as a solo worker</h1>
+						<br />
+						<form action="" class="d-flex flex-column gap-3">
+							<div class="row">
+								<div class="col-lg-6">
+									<label for="" class="fw-bold small">First Name</label>
+									<input type="text" class="form-control" />
+								</div>
+								<div class="col-lg-6">
+									<label for="" class="fw-bold small">Last Name</label>
+									<input type="text" class="form-control" />
+								</div>
+							</div>
+							<div class="row gap-3">
+								<div class="col-lg-12">
+									<label for="" class="fw-bold small">Contact Number</label>
+									<input type="text" class="form-control" />
+								</div>
+								<div class="col-lg-12">
+									<label for="" class="fw-bold small">Email Address</label>
+									<input type="email" class="form-control" />
+								</div>
+								<div class="col-lg-12">
+									<label for="" class="fw-bold small">Password</label>
+									<input type="password" class="form-control" />
+								</div>
+								<div class="col-lg-12">
+									<label for="" class="fw-bold small">Address</label>
+									<textarea name="" id="" cols="30" rows="10" class="form-control"></textarea>
+								</div>
+								<p class="small fst-italic">By registering on our website, you agree to the collection, use, and disclosure of your personal information to send you information about our services and promotions and to assist in booking our services.</p>
+								<div class="d-flex justify-content-between">
+									<div></div>
+									<button class="btn btn-primary text-uppercase fw-bold">Register</button>
+								</div>
+							</div>
+						</form>
+					</div>
+				</div>
+				<div class="position-relative col-md-5 p-0" style="background-color: #3fd4f8">
+					<img class="bottom-0 end-0 position-absolute" src="{{ asset('svg/undraw-register.svg') }}" alt="" width="95%" />
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+@endsection
