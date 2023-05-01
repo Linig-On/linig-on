73c778b3d7bac19423cc7fbf1cf8f6250189e1bf
Camela Kim Quidip <geumjassi74@gmail.com>
2023-05-01 18:01:28 +0800

    fixed WorkerRatingsSeeder user_id reference
    ++renamed file

--- database/seeders/WorkerRatingsSeeder.php
+++ database/seeders/WorkerRatingsSeeder.php
@@ -24,7 +24,7 @@ class WorkerRatings extends Seeder
         DB::statement('SET FOREIGN_KEY_CHECKS=1');
         $data = [
             [
-                'user_id' => 6,
+                'user_id' => 2,
                 'worker_id' =>  1,
                 'name' => 'Prince Jobert',
                 'comment' => 'They cleaned the place well and left the area spotless!',
@@ -33,7 +33,7 @@ class WorkerRatings extends Seeder
                 
             ],
             [
-                'user_id' => 6,
+                'user_id' => 3,
                 'worker_id' =>  1,
                 'name' => 'Joems Boletic',
                 'comment' => 'They were really professional in handling every items. Not to mention they really cleaned the place well',
@@ -42,7 +42,7 @@ class WorkerRatings extends Seeder
                 
             ],
             [
-                'user_id' => 6,
+                'user_id' => 1,
                 'worker_id' =>  1,
                 'name' => 'Mark Niño Alden',
                 'comment' => 'I appreciate their attention to detail and the fact that they always leave my home looking spotless.',
@@ -51,7 +51,7 @@ class WorkerRatings extends Seeder
                 
             ],
             [
-                'user_id' => 6,
+                'user_id' => 4,
                 'worker_id' =>  1,
                 'name' => 'Denise Menubo',
                 'comment' => 'I was not happy with their attention to detail. The floors were still dirty, and there was dust on the furniture.',
@@ -60,7 +60,7 @@ class WorkerRatings extends Seeder
                 
             ],
             [
-                'user_id' => 6,
+                'user_id' => 5,
                 'worker_id' =>  1,
                 'name' => 'Doms Esperida',
                 'comment' => 'I had to remind the cleaner to clean certain areas, and even then, the cleaning was not thorough',
@@ -70,7 +70,7 @@ class WorkerRatings extends Seeder
             ],
             // --worker 1 end--
             [
-                'user_id' => 7,
+                'user_id' => 4,
                 'worker_id' =>  2,
                 'name' => 'Denise Menubo',
                 'comment' => 'They did an incredible job cleaning my home. Everything was spotless and smelled fresh.',
@@ -79,7 +79,7 @@ class WorkerRatings extends Seeder
                 
             ],
             [
-                'user_id' => 7,
+                'user_id' => 1,
                 'worker_id' =>  2,
                 'name' => 'Mark Niño Alden',
                 'comment' => 'Their cleaning service is amazing! They always go above and beyond to make sure my home is clean and organized.',
@@ -88,7 +88,7 @@ class WorkerRatings extends Seeder
                 
             ],
             [
-                'user_id' => 7,
+                'user_id' => 5,
                 'worker_id' =>  2,
                 'name' => 'Doms Esperida',
                 'comment' => 'Their work was thorough, but I did notice a few areas that could have been improved.',
@@ -97,7 +97,7 @@ class WorkerRatings extends Seeder
                 
             ],
             [
-                'user_id' => 7,
+                'user_id' => 2,
                 'worker_id' =>  2,
                 'name' => 'Prince Jobert',
                 'comment' => 'Their service was average, but I expected a little more for the price.',
@@ -106,7 +106,7 @@ class WorkerRatings extends Seeder
                 
             ],
             [
-                'user_id' => 7,
+                'user_id' => 3,
                 'worker_id' =>  2,
                 'name' => 'Joems Boletic',
                 'comment' => 'They did a good job overall, but there were a few areas that needed more attention.',
@@ -116,7 +116,7 @@ class WorkerRatings extends Seeder
             ],
             // -- worker 2 end--
             [
-                'user_id' => 8,
+                'user_id' => 5,
                 'worker_id' =>  3,
                 'name' => 'Doms Esperida',
                 'comment' => 'Their cleaning service was okay, but they missed a few spots that I pointed out to them. Hopefully they improve next time..',
@@ -125,7 +125,7 @@ class WorkerRatings extends Seeder
                 
             ],
             [
-                'user_id' => 8,
+                'user_id' => 4,
                 'worker_id' =>  3,
                 'name' => 'Denis Menubo',
                 'comment' => 'Their cleaning was okay, but there were some areas that needed more work.',
@@ -134,7 +134,7 @@ class WorkerRatings extends Seeder
                 
             ],
             [
-                'user_id' => 8,
+                'user_id' => 3,
                 'worker_id' =>  3,
                 'name' => 'Joems Boletic',
                 'comment' => 'Their cleaning was mediocre, but it was done in a timely manner.',
@@ -143,7 +143,7 @@ class WorkerRatings extends Seeder
                 
             ],
             [
-                'user_id' => 8,
+                'user_id' => 2,
                 'worker_id' =>  3,
                 'name' => 'Prince Jobert',
                 'comment' => 'The cleaners arrived late and seemed rushed during the cleaning. They did an okay job, but I was expecting more..',
@@ -152,7 +152,7 @@ class WorkerRatings extends Seeder
                 
             ],
             [
-                'user_id' => 8,
+                'user_id' => 1,
                 'worker_id' =>  3,
                 'name' => 'Mark Alden',
                 'comment' => 'Ako na lang kuta nag linig',
@@ -162,7 +162,7 @@ class WorkerRatings extends Seeder
             ],
             // -- worker 3 end-- 
             [
-                'user_id' => 9,
+                'user_id' => 1,
                 'worker_id' =>  4,
                 'name' => 'Mark Alden',
                 'comment' => 'They were polite and professional, and their cleaning was good overall.',
@@ -171,7 +171,7 @@ class WorkerRatings extends Seeder
                 
             ],
             [
-                'user_id' => 9,
+                'user_id' => 2,
                 'worker_id' =>  4,
                 'name' => 'Prince Jobert',
                 'comment' => 'They were on time and efficient, and did a fantastic job cleaning my home. I would highly recommend them.',
@@ -180,7 +180,7 @@ class WorkerRatings extends Seeder
                 
             ],
             [
-                'user_id' => 9,
+                'user_id' => 3,
                 'worker_id' =>  4,
                 'name' => 'Joems Boletic',
                 'comment' => 'Their service was average, but there were some minor issues that needed addressing.',
@@ -189,7 +189,7 @@ class WorkerRatings extends Seeder
                 
             ],
             [
-                'user_id' => 9,
+                'user_id' => 4,
                 'worker_id' =>  4,
                 'name' => 'Denis Menubo',
                 'comment' => 'Their cleaning was satisfactory, but there were some areas that needed more work.',
@@ -198,7 +198,7 @@ class WorkerRatings extends Seeder
                 
             ],
             [
-                'user_id' => 9,
+                'user_id' => 5,
                 'worker_id' =>  4,
                 'name' => 'Doms Esperida',
                 'comment' => 'I was generally pleased with their service, but there were some minor issues..',
@@ -217,7 +217,7 @@ class WorkerRatings extends Seeder
                 
             ],
             [
-                'user_id' => 10,
+                'user_id' => 4,
                 'worker_id' =>  5,
                 'name' => 'Denis Menubo',
                 'comment' => 'I was generally satisfied with their service, but there were a few issues that needed addressing.',
@@ -226,7 +226,7 @@ class WorkerRatings extends Seeder
                 
             ],
             [
-                'user_id' => 10,
+                'user_id' => 3,
                 'worker_id' =>  5,
                 'name' => 'Joems Boletic',
                 'comment' => 'They did an okay job overall, but there were some spots that needed more attention.',
@@ -235,7 +235,7 @@ class WorkerRatings extends Seeder
                 
             ],
             [
-                'user_id' => 10,
+                'user_id' => 2,
                 'worker_id' =>  5,
                 'name' => 'Prince Jobert',
                 'comment' => 'I was not too happy with their cleaning service. They missed a lot of spots, and I had to point them out and ask them to clean it again.',
@@ -244,7 +244,7 @@ class WorkerRatings extends Seeder
                 
             ],
             [
-                'user_id' => 10,
+                'user_id' => 1,
                 'worker_id' =>  5,
                 'name' => 'Mark Alden',
                 'comment' => 'Their cleaning was good, but there were some areas that needed a little more attention.',
@@ -254,7 +254,7 @@ class WorkerRatings extends Seeder
             ],
             // -- worker 5 end-- 
             [
-                'user_id' => 14,
+                'user_id' => 1,
                 'worker_id' =>  6,
                 'name' => 'Mark Alden',
                 'comment' => 'They did a fantastic job cleaning my kitchen and bathroom. I will definitely be using their service again.',
@@ -263,7 +263,7 @@ class WorkerRatings extends Seeder
                 
             ],
             [
-                'user_id' => 14,
+                'user_id' => 2,
                 'worker_id' =>  6,
                 'name' => 'Prince Jobert',
                 'comment' => ' I am so impressed with their attention to detail. They really take pride in their work.',
@@ -272,7 +272,7 @@ class WorkerRatings extends Seeder
                 
             ],
             [
-                'user_id' => 14,
+                'user_id' => 3,
                 'worker_id' =>  6,
                 'name' => 'Joems Boletic',
                 'comment' => 'I was not completely satisfied with their service, but it was okay.',
@@ -281,7 +281,7 @@ class WorkerRatings extends Seeder
                 
             ],
             [
-                'user_id' => 14,
+                'user_id' => 4,
                 'worker_id' =>  6,
                 'name' => 'Denis Menubo',
                 'comment' => 'They did a decent job, but there were a few things that could have been better.',
@@ -290,7 +290,7 @@ class WorkerRatings extends Seeder
                 
             ],
             [
-                'user_id' => 14,
+                'user_id' => 5,
                 'worker_id' =>  6,
                 'name' => 'Doms Esperida',
                 'comment' => 'They are always professional and courteous. I trust them completely with my home.',
